ec07ba02c88433a45099c295ef7e5a1e9bc1db3c
pyf0304 <pyf0304@gmail.com>
2025-08-10 03:32:30 +0800

    fix(api): handle null response in getUser

--- src/ts/L0Entity/PrjFunction/clsTabFunctionPropEN.ts
+++ src/ts/L0Entity/PrjFunction/clsTabFunctionPropEN.ts
@@ -1,12 +1,12 @@
 ﻿/**
  * 类名:clsTabFunctionPropEN
  * 表名:TabFunctionProp(00050522)
- * 版本:2025.06.13.1(服务器:WIN-SRV103-116)
- * 日期:2025/06/14 11:50:34
+ * 版本:2025.08.02.1(服务器:PYF-AI)
+ * 日期:2025/08/05 01:02:24
  * 生成者:pyf
  工程名称:AGC(0005)
  CM工程:AgcSpa前端(000046, 变量首字母小写)-WebApi函数集
- * 相关数据库:103.116.76.183,8433AGC_CS12
+ * 相关数据库:109.244.40.104,8433AGC_CS12
  * PrjDataBaseId:0005
  模块中文名:函数管理(PrjFunction)
  * 框架-层名:实体层(TS)(EntityLayer,0121)
@@ -20,15 +20,15 @@ import { clsGeneralTab } from '@/ts/PubFun/clsGeneralTab';
 
 export class clsTabFunctionPropEN extends clsGeneralTab {
   public static _RefreshTimeLst = new Array<string>();
-  public static CacheAddiCondition = ''; //缓存附加条件,作为向后台调取数据的附加条件
-  public static CacheModeId = '04'; //sessionStorage
-  public static PrimaryTypeId = '02'; //identity
-  public static IsUseDelSign = false; //使用删除标志,记录不能删除,仅设置删除标志
-  public static WhereFormat = ''; //条件格式串
-  public static _CurrTabName = 'TabFunctionProp'; //当前表名,与该类相关的表名
-  public static _KeyFldName = 'mId'; //当前表中的关键字名称,与该类相关的表中关键字名
-  public static mintAttributeCount = 12;
-  public static AttributeName = [
+  public static _CacheAddiCondition = ''; //缓存附加条件,作为向后台调取数据的附加条件
+  public static readonly _CacheModeId: string = '04'; //sessionStorage
+  public static readonly _PrimaryTypeId: string = '02'; //identity
+  public static readonly _IsUseDelSign = false; //使用删除标志,记录不能删除,仅设置删除标志
+  public static readonly _WhereFormat = ''; //条件格式串
+  public static readonly _CurrTabName: string = 'TabFunctionProp'; //当前表名,与该类相关的表名
+  public static readonly _KeyFldName: string = 'mId'; //当前表中的关键字名称,与该类相关的表中关键字名
+  public static readonly _AttributeCount = 12;
+  public static readonly _AttributeName = [
     'mId',
     'tabId',
     'functionTemplateId',
@@ -350,97 +350,73 @@ export class clsTabFunctionPropEN extends clsGeneralTab {
    * 常量:"mId"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_mId(): string {
-    return 'mId';
-  } //mId
+  public static readonly con_mId = 'mId'; //mId
 
   /**
    * 常量:"TabId"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_TabId(): string {
-    return 'tabId';
-  } //表ID
+  public static readonly con_TabId = 'tabId'; //表ID
 
   /**
    * 常量:"FunctionTemplateId"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_FunctionTemplateId(): string {
-    return 'functionTemplateId';
-  } //函数模板Id
+  public static readonly con_FunctionTemplateId = 'functionTemplateId'; //函数模板Id
 
   /**
    * 常量:"CodeTypeId"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_CodeTypeId(): string {
-    return 'codeTypeId';
-  } //代码类型Id
+  public static readonly con_CodeTypeId = 'codeTypeId'; //代码类型Id
 
   /**
    * 常量:"FuncId4GC"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_FuncId4GC(): string {
-    return 'funcId4GC';
-  } //函数ID
+  public static readonly con_FuncId4GC = 'funcId4GC'; //函数ID
 
   /**
    * 常量:"MethodModifierId"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_MethodModifierId(): string {
-    return 'methodModifierId';
-  } //函数修饰语Id
+  public static readonly con_MethodModifierId = 'methodModifierId'; //函数修饰语Id
 
   /**
    * 常量:"IsForAllTemplate"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_IsForAllTemplate(): string {
-    return 'isForAllTemplate';
-  } //是否针对所有模板
+  public static readonly con_IsForAllTemplate = 'isForAllTemplate'; //是否针对所有模板
 
   /**
    * 常量:"OrderNum"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_OrderNum(): string {
-    return 'orderNum';
-  } //序号
+  public static readonly con_OrderNum = 'orderNum'; //序号
 
   /**
    * 常量:"PrjId"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_PrjId(): string {
-    return 'prjId';
-  } //工程Id
+  public static readonly con_PrjId = 'prjId'; //工程Id
 
   /**
    * 常量:"UpdDate"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_UpdDate(): string {
-    return 'updDate';
-  } //修改日期
+  public static readonly con_UpdDate = 'updDate'; //修改日期
 
   /**
    * 常量:"UpdUser"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_UpdUser(): string {
-    return 'updUser';
-  } //修改者
+  public static readonly con_UpdUser = 'updUser'; //修改者
 
   /**
    * 常量:"Memo"
    * (AutoGCLib.EntityLayer4TypeScript:Gen_EN_PropertyNameConst)
    */
-  public static get con_Memo(): string {
-    return 'memo';
-  } //说明
+  public static readonly con_Memo = 'memo'; //说明
 
   /**
    * 设置条件字段值.
@@ -459,6 +435,7 @@ export class clsTabFunctionPropEN extends clsGeneralTab {
     }
     this.sfFldComparisonOp = JSON.stringify(this.dicFldComparisonOp);
   }
+
   /**
    * 判断一个字符串是否是类的属性
    * @param propName: 属性名
